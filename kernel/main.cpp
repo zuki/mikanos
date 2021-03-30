eed79b3d3c84f5495f80cba4eb1e205892de194d
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2021-03-30 19:02:06 +0900

    change for day05e

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -10,6 +10,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
+#include "console.hpp"
 
 void* operator new(size_t size, void *buf) {
     return buf;
@@ -40,22 +41,12 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
         }
     }
 
-    for (int x = 0; x < 200; ++x) {
-        for (int y = 0; y < 100; ++y) {
-            pixel_writer->Write(x, y, {0, 255, 0});
-        }
-    }
-
-    int i = 0;
-    for (char c = '!'; c <= '~'; ++c, ++i) {
-        WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-    }
-
-    WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
-
+    Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
     char buf[128];
-    sprintf(buf, "1 + 2 = %d", 1 + 2);
-    WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
+    for (int i = 0; i < 27; ++i) {
+        sprintf(buf, "line %d\n", i);
+        console.PutString(buf);
+    }
 
     while (1) __asm__("hlt");
 }
