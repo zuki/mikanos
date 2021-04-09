afa237c21a4393d1971e31f68c1958b39fc5c34d
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-09 16:32:38 +0900

    change for day09b

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -32,6 +32,7 @@
 #include "memory_manager.hpp"
 #include "window.hpp"
 #include "layer.hpp"
+#include "timer.hpp"
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
@@ -60,7 +61,11 @@ unsigned int mouse_layer_id;
 void MouseObserver(int8_t displacement_x, int8_t displacement_y)
 {
     layer_manager->MoveRelative(mouse_layer_id, {displacement_x, displacement_y});
+    StartLAPICTimer();
     layer_manager->Draw();
+    auto elapsed = LAPICTimerElapsed();
+    StopLAPICTimer();
+    printk("MouseObserver: elapsed = %u\n", elapsed);
 }
 
 void SwitchEhci2Xhci(const pci::Device &xhc_dev)
@@ -130,6 +135,8 @@ extern "C" void KernelMainNewStack(
     console->SetWriter(pixel_writer);
     printk("Welcome to MikanOS!\n");
     SetLogLevel(kWarn);
+
+    InitializeLAPICTimer();
 
     SetupSegments();
 
