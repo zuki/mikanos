08eab61ff2218cc5c1a8ff67d7f51ee7db35dfd1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-12 10:33:12 +0900

    change for day10c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -275,10 +275,8 @@ extern "C" void KernelMainNewStack(
     mouse_position = {200, 200};
 
     auto main_window = std::make_shared<Window>(
-        160, 68, frame_buffer_config.pixel_format);
+        160, 52, frame_buffer_config.pixel_format);
     DrawWindow(*main_window->Writer(), "Hello Window");
-    WriteString(*main_window->Writer(), {24, 28}, "Welcome to", {0, 0, 0});
-    WriteString(*main_window->Writer(), {24, 44}, " MikanOS world!", {0, 0, 0});
 
     FrameBuffer screen;
     if (auto err = screen.Initialize(frame_buffer_config)) {
@@ -307,10 +305,19 @@ extern "C" void KernelMainNewStack(
     layer_manager->UpDown(main_window_layer_id, 1);
     layer_manager->Draw();
 
+    char str[128];
+    unsigned int count = 0;
+
     while (true) {
+        ++count;
+        sprintf(str, "%010u", count);
+        FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
+        WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
+        layer_manager->Draw();
+
         __asm__("cli");
         if (main_queue.Count() == 0) {
-            __asm__("sti\n\thlt");
+            __asm__("sti");
             continue;
         }
 
