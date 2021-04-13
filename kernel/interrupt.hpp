c794b6dc7ba2bc4fbe953795000e20d8b0824774
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-13 11:17:27 +0900

    change for day11b

--- kernel/interrupt.hpp
+++ kernel/interrupt.hpp
@@ -59,6 +59,7 @@ class InterruptVector {
 public:
     enum Number {
         kXHCI = 0x40,
+        kLAPICTimer = 0x41,
     };
 };
 
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -30,6 +30,7 @@
 #include "window.hpp"
 #include "layer.hpp"
 #include "message.hpp"
+#include "timer.hpp"
 
 int printk(const char *format, ...) {
     va_list ap;
@@ -90,6 +91,8 @@ extern "C" void KernelMainNewStack(
     InitializeMouse();
     layer_manager->Draw({{0, 0}, ScreenSize()});
 
+    InitializeLAPICTimer();
+
     char str[128];
     unsigned int count = 0;
 
@@ -113,6 +116,9 @@ extern "C" void KernelMainNewStack(
         switch (msg.type) {
             case Message::kInterruptXHCI:
                 usb::xhci::ProcessEvents();
+                break;
+            case Message::kInterruptLAPICTimer:
+                printk("Timer interrupt\n");
                 break;
             default:
                 Log(kError, "Unknown message type: %d\n", msg.type);
