1cfc2ce835af373c7cdfdace0bdf331512532b8f
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-14 09:49:48 +0900

    change for day12c

---
+++ kernel/keyboard.cpp
@@ -0,0 +1,28 @@
+#include "keyboard.hpp"
+
+#include <memory>
+#include "usb/classdriver/keyboard.hpp"
+
+namespace {
+    const char keycode_map[256] = {
+        0,   0,     0,    0,    'a',  'b', 'c', 'd', // 0
+        'e', 'f',   'g',  'h',  'i',  'j', 'k', 'l', // 8
+        'm', 'n',   'o',  'p',  'q',  'r', 's', 't', // 16
+        'u', 'v',   'w',  'x',  'y',  'z', '1', '2', // 24
+        '3', '4',   '5',  '6',  '7',  '8', '9', '0', // 32
+        '\n', '\b', 0x08, '\t', ' ',  '-', '=', '[', // 40
+        ']',  '\\', '#',  ';',  '\'', '`', ',', '.', // 48
+        '/',  0,    0,    0,    0,    0,   0,   0, // 56
+    };
+}
+
+void InitializeKeyboard(std::deque<Message> &msg_queue)
+{
+    usb::HIDKeyboardDriver::default_observer =
+        [&msg_queue](uint8_t keycode) {
+            Message msg{Message::kKeyPush};
+            msg.arg.keyboard.keycode = keycode;
+            msg.arg.keyboard.ascii = keycode_map[keycode];
+            msg_queue.push_back(msg);
+        };
+}
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -32,6 +32,7 @@
 #include "message.hpp"
 #include "timer.hpp"
 #include "acpi.hpp"
+#include "keyboard.hpp"
 
 int printk(const char *format, ...) {
     va_list ap;
@@ -96,8 +97,7 @@ extern "C" void KernelMainNewStack(
     acpi::Initialize(acpi_table);
     InitializeLAPICTimer(*main_queue);
 
-    timer_manager->AddTimer(Timer(200, 2));
-    timer_manager->AddTimer(Timer(600, -1));
+    InitializeKeyboard(*main_queue);
 
     char str[128];
 
@@ -126,11 +126,10 @@ extern "C" void KernelMainNewStack(
                 usb::xhci::ProcessEvents();
                 break;
             case Message::kTimerTimeout:
-                printk("Timer: timeout = %lu, value = %d\n",
-                    msg.arg.timer.timeout, msg.arg.timer.value);
-                if (msg.arg.timer.value > 0) {
-                    timer_manager->AddTimer(
-                        Timer(msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+                break;
+            case Message::kKeyPush:
+                if (msg.arg.keyboard.ascii != 0) {
+                    printk("%c", msg.arg.keyboard.ascii);
                 }
                 break;
             default:
