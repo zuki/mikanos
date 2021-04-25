5c85881246fb3d84148b12760c8e0a24011cecd4
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-25 15:37:24 +0900

    change for day20e

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,5 +1,8 @@
 #include <cstring>
 #include <cstdlib>
+#include <cstdint>
+
+#include "../../kernel/logger.hpp"
 
 int stack_ptr;
 long stack[100];
@@ -17,6 +20,8 @@ void Push(long value)
     stack[stack_ptr] = value;
 }
 
+extern "C" int64_t SyscallLogString(LogLevel, const char *);
+
 extern "C" int main(int argc, char **argv)
 {
     stack_ptr = -1;
@@ -26,19 +31,23 @@ extern "C" int main(int argc, char **argv)
             long b = Pop();
             long a = Pop();
             Push(a + b);
+            SyscallLogString(kWarn, "+");
         } else if (strcmp(argv[i], "-") == 0) {
             long b = Pop();
             long a = Pop();
             Push(a - b);
+            SyscallLogString(kWarn, "-");
         } else {
             long a = atol(argv[i]);
             Push(a);
+            SyscallLogString(kWarn, "#");
         }
     }
 
     if (stack_ptr < 0) {
         return 0;
     }
+    SyscallLogString(kWarn, "\nhello, this is rpn\n");
     while (1);
     //return static_cast<int>(Pop());
 }
