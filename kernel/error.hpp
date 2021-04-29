c74c92e97ab432dd95237c5395867f8cd913d419
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-29 11:19:34 +0900

    change for day24g

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -31,6 +31,7 @@ public:
         kNoSuchTask,
         kInvalidFormat,
         kFrameTooSmall,
+        kInvalidFile,
         kLastOfCode,    // この列挙子は常に最後に配置する
     };
 
@@ -61,6 +62,7 @@ private:
         "kNoSuchTask",
         "kInvalidFormat",
         "kFrameTooSmall",
+        "kInvalidFile",
     };
     static_assert(Error::Code::kLastOfCode == code_names_.size());
 
