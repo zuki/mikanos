13e4b269a93329c07f7bdc0f2831b75883746615
Keiji Suzuki <zuki.ebetsu@gmail.com>
2021-04-30 09:51:39 +0900

    change for day25c

---
+++ apps/grep/grep.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include <regex>
+
+extern "C" void main(int argc, char **argv)
+{
+    if (argc < 3) {
+        printf("Usage: %s <patern> <file>\n", argv[0]);
+        exit(1);
+    }
+
+    std::regex pattern{argv[1]};
+
+    FILE *fp = fopen(argv[2], "r");
+    if (fp == nullptr) {
+        printf("failed to open: %s\n", argv[2]);
+        exit(1);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp)) {
+        std::cmatch m;
+        if (std::regex_search(line, m, pattern)) {
+            printf("%s", line);
+        }
+    }
+    exit(0);
+}
